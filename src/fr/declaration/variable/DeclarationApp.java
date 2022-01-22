21b64e9b298deb3df54cfc175d7bebb416b413fb
Anaïs Kominiarz <kominiarz.anais@gmail.com>
2022-01-22 17:39:41 +0100

    differents types

---
+++ src/fr/declaration/variable/DeclarationApp.java
@@ -0,0 +1,28 @@
+package fr.declaration.variable;
+
+public class DeclarationApp {
+
+	public static void main(String[] args) {
+		
+		byte varByte = 5;
+		short varShort = 458;
+		int varInt = 1544;
+		long varLong = 145587;
+		
+		float varFloat = 1.25F;
+		double varDouble = 12.552;
+		
+		char varChar = 'r';
+		
+		boolean varBoolean = true;
+		
+		String varString = "Hello World ! ";
+		
+		System.out.println(varBoolean); 
+		
+		String randomString = "Voici le résultat d'un calcul :\n 1+5=6";
+		
+		System.out.println(randomString);
+	}
+
+}
